49d6a15ce464761eecc44659c155e7b8c51d6eca
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-19 18:02:52 -0300

    Modelagem da entidade Utm

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,13 +1,23 @@
-﻿namespace UtmBuilder.Core
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
 {
     public class Utm
     {
-        public string Url { get; set; }
-        public string Source { get; set; }
-        public string Medium { get; set; }
-        public string Name { get; set; }
-        public string Id { get; set; }
-        public string Term { get; set; }
-        public string Content { get; set; }
+        public Utm(Url url, Campaign campaign)
+        {
+            Url = url;
+            Campaign = campaign;
+        }
+
+        /// <summary>
+        /// URL (Website link)
+        /// </summary>
+        public Url Url { get; }
+
+        /// <summary>
+        /// Campaign Details
+        /// </summary>
+        public Campaign Campaign { get; }
     }
 }

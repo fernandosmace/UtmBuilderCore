6a28f7c76dd7dc3a00b093a082799c3f05da6ccf
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-27 22:47:34 -0300

    Adiciona testes de Campaign

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,38 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+using Xunit;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    public class CampaignTests
+    {
+        [Fact(DisplayName = "Deve retornar uma exceção quando Source for inválido")]
+        [Trait("Category", "Campaign")]
+        public void ShouldReturnExceptionWhenSourceIsNull()
+        {
+            Assert.Throws<InvalidCampaignException>(() => new Campaign("", "teste", "teste"));
+        }
+
+        [Fact(DisplayName = "Deve retornar uma exceção quando Medium for inválido")]
+        [Trait("Category", "Campaign")]
+        public void ShouldReturnExceptionWhenMediumIsNull()
+        {
+            Assert.Throws<InvalidCampaignException>(() => new Campaign("teste", "", "teste"));
+        }
+
+        [Fact(DisplayName = "Deve retornar uma exceção quando Name for inválido")]
+        [Trait("Category", "Campaign")]
+        public void ShouldReturnExceptionWhenNameIsNull()
+        {
+            Assert.Throws<InvalidCampaignException>(() => new Campaign("teste", "teste", ""));
+        }
+
+        [Fact(DisplayName = "Deve retornar uma exceção quando Name for inválido")]
+        [Trait("Category", "Campaign")]
+        public void ShouldNotReturnExceptionWhenCampaignIsValid()
+        {
+            var exception = Record.Exception(() => new Campaign("teste", "teste", "teste"));
+            Assert.Null(exception);
+        }
+    }
+}
